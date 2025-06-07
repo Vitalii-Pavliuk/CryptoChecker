fdb781e6bce0b3749bd7089c642b540057b19942
Morello100 <114472069+Morello100@users.noreply.github.com>
2025-06-07 13:54:55 +0300

    Fix/gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CryptoChecker/',
+  base: './',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
