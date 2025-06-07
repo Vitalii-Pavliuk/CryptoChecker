abdea9b2ee605adfee541f720a83303c401e34e1
Morello100 <114472069+Morello100@users.noreply.github.com>
2025-06-07 13:58:52 +0300

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/CryptoChecker/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          charts: ['recharts']
+        }
       }
-    }
+    },
+    chunkSizeWarningLimit: 1000
   }
 })
